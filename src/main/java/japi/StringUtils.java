4303a2960a3a64bd87cc807142bfe89b1a2ee757
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 16:02:14 -0700

    Update StringUtils.java

--- src/main/java/japi/StringUtils.java
+++ src/main/java/japi/StringUtils.java
@@ -1,3 +1,17 @@
 package japi;
 
-public class StringUtils 
+public class StringUtils {
+  public static String[] getLetters(String line) {
+    return line.split("");
+  }
+  public static String compileSentence(String[] strings) {
+    String r = strings[0] + " ";
+    for (int i = 1; i < strings.length; i++) {    // YES, I used "1" intentionally. Think about it...  ~~John
+      r += " " + strings[i];
+    }
+    return r;
+  }
+  public static String getWords(String line) {
+    return line.split(" ");
+  }
+}
