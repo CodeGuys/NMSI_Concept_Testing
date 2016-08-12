3caf3e49b82e9a90b4b75a395fc676338999ce18
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:22:31 -0400

    Update Composition.java

--- src/main/java/jogen/game/world/object/data/composition/Composition.java
+++ src/main/java/jogen/game/world/object/data/composition/Composition.java
@@ -1,1 +1,13 @@
-public class Composition 
+public class Composition {
+  public ArrayList<Element> elements = new ArrayList<Element>();
+  public ArrayList<int> percents = new ArrayList<int>();
+  
+  public Composition() {
+    
+  }
+  
+  public void add(Element e, int p) {
+    elements.add(e);
+    percents.add(p);
+  }
+}
