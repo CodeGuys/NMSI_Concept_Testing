b4ad4718acdd119a1461f48bd048fa80e3ed98bd
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:53:49 -0400

    Update Planet.java

--- src/main/java/jogen/game/world/planet/Planet.java
+++ src/main/java/jogen/game/world/planet/Planet.java
@@ -1,1 +1,18 @@
-public class Planet 
+package jogen.game.world.planet;
+
+public class Planet {
+  public PlanetPos position;
+  
+  public String name;
+  public TerrainMap terrain;
+  
+  public Planet() {
+    this.name = name;
+    this.terrain = TerrainMap.generateNew(new Random().nextInt());
+  }
+  
+  public Planet generate(int seed) {
+    this.name = Planets.getRandomName();
+    this.terrain = TerrainMap.generateNew(seed);
+  }
+}
