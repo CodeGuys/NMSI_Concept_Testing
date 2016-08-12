88ea142c9967e9cfc4ccba41f178dc04e383e5de
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:59:35 -0400

    Update Planets.java

--- src/main/java/jogen/game/world/planet/Planets.java
+++ src/main/java/jogen/game/world/planet/Planets.java
@@ -1,1 +1,14 @@
-public class Planets 
+package jogen.game.world.planet;
+
+public class Planets {
+  public String[] classes = {
+    "Geoid"
+  }
+  public String[] names = {
+    "prekker"
+  }
+  
+  public String getRandomName() {
+    return JMath.rand(classes) + " - " + JMath.rand(names);
+  }
+}
