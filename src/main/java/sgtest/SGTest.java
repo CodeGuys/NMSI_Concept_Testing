46a82c80443def5a155dce2460cf8fb8134c8f38
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 11:05:04 -0400

    Update SGTest.java

--- src/main/java/sgtest/SGTest.java
+++ src/main/java/sgtest/SGTest.java
@@ -1,1 +1,10 @@
-public class SGTest 
+package sgtest;
+
+import jogen.game.Game;
+import jogen.game.world.World;
+
+public class SGTest {
+  public static void main(String[] args) {
+    World world = new World().generate(new Rand().nextInt();
+  }
+}
