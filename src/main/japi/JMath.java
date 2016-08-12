2b3d8c5b0fda9b1d2dd13d136e283240f7bb00ce
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:12:40 -0400

    Update JMath.java

--- src/main/japi/JMath.java
+++ src/main/japi/JMath.java
@@ -1,2 +1,17 @@
+package japi;
+
 public class JMath {
+  public static Object rand(Object[] objects) {
+    Random g = new Random();
+    return objects[g.nextInt(objects.length)];
+  }
+  public static String rand(String[] objects) {
+    Random g = new Random();
+    return objects[g.nextInt(objects.length)];
+  }
+  public static int rand(int min, int max) {
+    Random g = new Random();
+    int f = max-min;
+    return g.nextInt(f) + min;
+  }
 }
