b7a942c5ceb6f53ac6e05e86b96e63d982b9e606
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 15:54:52 -0700

    Update Element.java

--- src/main/java/jogen/game/world/Element.java
+++ src/main/java/jogen/game/world/Element.java
@@ -1,4 +1,5 @@
 // HI!
+// Hey! Just FYI, I'm either going to overwrite this code in a minute or so, or I'll create a new branch.
 public class Element {
   public String name;
   public int number;
