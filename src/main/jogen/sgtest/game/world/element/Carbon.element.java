347d359b909ad8ea32a195ee7d6af4dc5e2ece74
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:04:59 -0400

    Update Carbon.element.java

--- src/main/jogen/sgtest/game/world/element/Carbon.element.java
+++ src/main/jogen/sgtest/game/world/element/Carbon.element.java
@@ -1,7 +1,40 @@
 @Element {
   name "Hydrogen";
+  number 1;
+}
+@Element {
+  name "Helium";
+  number 2;
+}
+@Element {
+  name "Lithium";
+  number 3;
+}
+@Element {
+  name "Berylium";
+  number 4;
+}
+@Element {
+  name "Boron";
+  number 5;
 }
 @Element {
   name "Carbon";
-  number = 6;
+  number 6;
+}
+@Element {
+  name "Nitrogen";
+  number 7;
+}
+@Element {
+  name "Oxygen";
+  number 8;
+}
+@Element {
+  name "Fluorine";
+  number 9;
+}
+@Element {
+  name "Neon"
+  number 10;
 }
