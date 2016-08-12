fa2df2ad498a2174dbb6ed10b1f445fe9d2f8276
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 10:36:49 -0400

    Update Element.java

--- src/main/java/jogen/game/world/element/Element.java
+++ src/main/java/jogen/game/world/element/Element.java
@@ -1,3 +1,21 @@
 package jogen.game.world.element;
 
-public class Element
+public class Element {
+  public String name;
+  public String symbol;
+  public int number;
+  
+  public boolean radioactive;
+  public int strength;
+  
+  public Element() {
+    
+  }
+  
+  public Element generate() {
+    Element e = new Element();
+    e.name = rand(Elements_e.names);
+    e.symbol = Elements.getSymbol(e.name);
+    e.number = Elements.getNumber(e.number);
+  }
+}
